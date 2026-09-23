ea9a05ea33b61747ea9f325eacfad61fc0b1efae
Imoh Jimmy <hlccservice@gmail.com>
2026-09-23 07:59:59 +0100

    update service Commission

--- src/_nav.jsx
+++ src/_nav.jsx
@@ -120,6 +120,11 @@ const _nav = [
         name: 'Staff Penalty',
         to: '/staffPenalty',
       },
+      {
+        component: CNavItem,
+        name: 'Service Commission',
+        to: '/serviceCommission',
+      },
     ],
   },
   {
